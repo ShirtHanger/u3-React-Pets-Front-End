d64ff362e25f63613a5df6f6569595838c63b3ff
jordanjc-ShirtHanger <devvcarter4ever24@outlook.com>
2024-11-08 11:28:48 -0600

    init

---
+++ react-pets-front-end/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import BASE_URL from './services/petService';
+
+// src/App.jsx
+
+const App = () => {
+
+  return (
+
+  <>
+  <h1>Hello world!</h1>
+  <p>{ BASE_URL }</p>
+  </>
+ 
+
+)
+};
+
+export default App;
+
